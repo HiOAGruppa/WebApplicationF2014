201c1db43a2d7fb621699bcd0dd00e718f797c3c
Sondre S Boge <NaughtyLlama@users.noreply.github.com>
2014-10-06 21:08:50 +0200

    Implementet ItemViews for Store

--- WebAppH2014/Controllers/DbTestController.cs
+++ WebAppH2014/Controllers/DbTestController.cs
@@ -22,29 +22,29 @@ namespace WebAppH2014.Controllers
             //gets all users and salesitems
             var users = db.Users.ToList();
             var salesItems = db.SalesItems.Include(a => a.Genre).ToList();
-            //foreach (var user in users)
-            //{
+           /* foreach (var user in users)
+            {
 
-            //    //gets all orders belonging to current user
-            //    var orders = db.Orders.Where(it => it.ownerUser.UserId == user.UserId).ToList();
-            //    Debug.WriteLine("Orders found");
+                //gets all orders belonging to current user
+                var orders = db.Orders.Where(it => it.ownerUser.UserId == user.UserId).ToList();
+                Debug.WriteLine("Orders found");
 
-            //    foreach (var order in orders)
-            //    {
-            //        //adds the list of items in the order
-            //        var orderSalesItem = db.SalesItemInOrder.Where(it => it.OrderId == order.OrderId).ToList();
-            //        foreach(var item in orderSalesItem)
-            //        {
-            //            //couples the items in the order, to a salesItem-object which will give us the item-info
-            //            var salesitem = salesItems.Where(it => it.SalesItemId == item.SalesItemId).FirstOrDefault();
-            //            item.SalesItem = salesitem;
-            //        }
-            //        //adds all the orderItems to the order
-            //        order.SalesItems = orderSalesItem;
-            //    }
-            //    //adds the orders to the user
-            //    user.Orders = orders;
-            //}
+                foreach (var order in orders)
+                {
+                    //adds the list of items in the order
+                    var orderSalesItem = db.SalesItemInOrder.Where(it => it.OrderId == order.OrderId).ToList();
+                    foreach(var item in orderSalesItem)
+                    {
+                        //couples the items in the order, to a salesItem-object which will give us the item-info
+                        var salesitem = salesItems.Where(it => it.SalesItemId == item.SalesItemId).FirstOrDefault();
+                        item.SalesItem = salesitem;
+                    }
+                    //adds all the orderItems to the order
+                    order.SalesItems = orderSalesItem;
+                }
+                //adds the orders to the user
+                user.Orders = orders;
+            }*/
             List<User> theseusers = new List<User>();
             foreach (var user in users)
             {
@@ -57,31 +57,7 @@ namespace WebAppH2014.Controllers
 
         public ActionResult Stock()
         {
-            var users = db.Users.ToList();
             var salesItems = db.SalesItems.ToList();
-            foreach (var user in users)
-            {
-
-                //gets all orders belonging to current user
-                var orders = db.Orders.Where(it => it.ownerUser.UserId == user.UserId).ToList();
-                Debug.WriteLine("Orders found");
-
-                foreach (var order in orders)
-                {
-                    //adds the list of items in the order
-                    var orderSalesItem = db.SalesItemInOrder.Where(it => it.OrderId == order.OrderId).ToList();
-                    foreach (var item in orderSalesItem)
-                    {
-                        //couples the items in the order, to a salesItem-object which will give us the item-info
-                        var salesitem = salesItems.Where(it => it.SalesItemId == item.SalesItemId).FirstOrDefault();
-                        item.SalesItem = salesitem;
-                    }
-                    //adds all the orderItems to the order
-                    order.SalesItems = orderSalesItem;
-                }
-                //adds the orders to the user
-                user.Orders = orders;
-            }
             return View(salesItems);
         }
     }
--- WebAppH2014/Models/StoreInitializer.cs
+++ WebAppH2014/Models/StoreInitializer.cs
@@ -47,36 +47,47 @@ namespace WebAppH2014.Models
             {
                 new Genre{Name="Data", Description="Stuff for data"},
 
-                new Genre{Name="TV", Description="Stuff for tv"}
+                new Genre{Name="Music", Description="Stuff for music"}
 
             };
 
             var salesItems = new List<SalesItem>
             {
                 //id 1
-                new SalesItem{Name ="Keyboard-Red", Price=200, Description="Sykt stilig keyboard", InStock=10, Genre = genres.Single(g=>g.Name=="Data")},
+                new SalesItem{Name ="Keyboard - Red", Price=200, Description="Sykt rødt og stilig keyboard!", InStock=10, Genre = genres.Single(g=>g.Name=="Data")},
                 //id 2
-                new SalesItem{Name ="Keyboard-Black", Price=250, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
-                //id 1
-                new SalesItem{Name ="Keyboard-Red", Price=200, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
-                //id 2
-                new SalesItem{Name ="Keyboard-Black", Price=300, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
-                                //id 1
-                new SalesItem{Name ="Keyboard-Red", Price=150, Description="Sykt billig og stilig keyboard, som bare varer og varer og varer og varer enda lenger. Den snille prisen gjenspeiler hvor awesome denne bedriften er og vi håper å kunne fortsette å holde det slik.", InStock=10,Genre = genres.Single(g=>g.Name=="TV")},
-                //id 2
-                new SalesItem{Name ="Keyboard-Black", Price=200, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
-                                //id 1
-                new SalesItem{Name ="Keyboard-Red", Price=700, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
-                //id 2
-                new SalesItem{Name ="Keyboard-Black", Price=200, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="TV")},
-                                //id 1
-                new SalesItem{Name ="Keyboard-Red", Price=300, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
-                //id 2
-                new SalesItem{Name ="Keyboard-Black", Price=200, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
-                                //id 1
-                new SalesItem{Name ="Keyboard-Red", Price=1000, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="TV")},
-                //id 2
-                new SalesItem{Name ="Keyboard-Black", Price=2000, Description="Sykt stilig keyboard", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                new SalesItem{Name ="Keyboard - Black", Price=250, Description="Sykt stilig svart keyboard!", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 3
+                new SalesItem{Name ="Commandore 64", Price=200, Description="Sykt gammelt, men stilig keyboard!", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 4
+                new SalesItem{Name ="Keyboard - Android", Price=300, Description="Anroid, what more to say?", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 5
+                new SalesItem{Name ="Keyboard - Bended", Price=150, Description="Sykt billig og stilig og ikke minst bøyelig keyboard, som bare varer og varer og varer og varer enda lenger. Den snille prisen gjenspeiler hvor awesome denne bedriften er og vi håper å kunne fortsette å holde det slik.", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 6
+                new SalesItem{Name ="Keyboard - Drawing", Price=200, Description="Et fint lite tastatur konsept, obs bare tegning er inkl.", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 7
+                new SalesItem{Name ="Keyboard - Enthusiast", Price=700, Description="For en som har masse penger og vil at ting skal se dyrt ut. En slags futuristisk look.", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 8
+                new SalesItem{Name ="Keyboard - Futuristic", Price=200, Description="Det egentlige futuristiske tastaturet på Tast-En-Tur. Vårt kjære lille barn som vi alltid vil elske.", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 9
+                new SalesItem{Name ="Keyboard - Half Wierd", Price=300, Description="Et halvveis normalt keyboard, som passer mødre som vil føle seg moderne.", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 10
+                new SalesItem{Name ="Keyboard - Modern Rainbow", Price=200, Description="Moderne og fargerikt!", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 11
+                new SalesItem{Name ="Keyboard - No Case", Price=1000, Description="Om du ikke liker plastikk, men elsker elektronikk.", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 12
+                new SalesItem{Name ="Keyboard - Rainbow", Price=2000, Description="Nå slipper du å ta drugs!", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 13
+                new SalesItem{Name ="Keyboard - Right Handed", Price=2000, Description="Om venstre hånda di er opptatt...", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 14
+                new SalesItem{Name ="Keyboard - Virtual", Price=1000, Description="Nå trenger du ikke klunkete tastatur, hvorfor ikke slå ihjel fingrene på bordet ditt?", InStock=10,Genre = genres.Single(g=>g.Name=="Data")},
+                //id 15
+                new SalesItem{Name ="Musical Keyboard - Retro", Price=1000, Description="Musikk, for de som ikke føler seg like unge lenger, og savner de gode gamle dager.", InStock=10,Genre = genres.Single(g=>g.Name=="Music")},
+                //id 16
+                new SalesItem{Name ="Musical Keyboard with iPad", Price=200, Description="Om du mangler både keyboard og overprisa en iPad.", InStock=10,Genre = genres.Single(g=>g.Name=="Music")},
+                //id 17
+                new SalesItem{Name ="Musical Keyboard", Price=700, Description="Standard utgaven for musikk keyboardene.", InStock=10,Genre = genres.Single(g=>g.Name=="Music")},
+                
             };
 
             foreach (var vare in salesItems)
