295f5c8918cf6b3eb3ed5adfba84a0d13c32589c
Martin Hagen <martin.hagenjr@hotmail.com>
2014-10-06 12:25:41 +0200

    Test commit, added storecontroller

---
+++ WebAppH2014/Controllers/StoreController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppH2014.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
