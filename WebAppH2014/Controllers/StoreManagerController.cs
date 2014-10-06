8bf7eebf21cb6f4d7063633d1e28a5edcbd69f5c
Martin Hagen <martin.hagenjr@hotmail.com>
2014-10-06 16:08:04 +0200

    started on a admin view
    for add, edit and delete on items

---
+++ WebAppH2014/Controllers/StoreManagerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using WebAppH2014.Models;
+
+namespace WebAppH2014.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        private StoreContext db = new StoreContext();
+        //
+        // GET: /StoreManager/
+        public ActionResult Index()
+        {
+            var items = db.SalesItems.Include(a => a.Genre);
+            return View(items.ToList());
+        }
+	}
+}
