5e98f0b7d7da85ff14c7b79ca8e9b15f9327668a
Martin Hagen <martin.hagenjr@hotmail.com>
2014-10-10 23:41:16 +0200

    fixed bug where imageUrl was blank after edit

--- WebAppH2014/Controllers/StoreManagerController.cs
+++ WebAppH2014/Controllers/StoreManagerController.cs
@@ -93,6 +93,7 @@ namespace WebAppH2014.Controllers
         {
             if (ModelState.IsValid)
             {
+                item.ImageUrl = item.Name;
                 db.Entry(item).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
