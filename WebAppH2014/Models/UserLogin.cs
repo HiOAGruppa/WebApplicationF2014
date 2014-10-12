f3b590b98272f3bd6262731fd84744c41db39ee0
NaughtyLlama <sondreboge@hotmail.com>
2014-10-12 14:59:57 +0200

    Fixed bug with duplicate users

--- WebAppH2014/Models/UserLogin.cs
+++ WebAppH2014/Models/UserLogin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -11,7 +12,11 @@ namespace WebAppH2014.Models
 
         [Key]
         public int UserId { get; set; }
+        [StringLength(450)]
+        [Index("UserNameIndex", IsUnique = true)]
+        [Required(ErrorMessage = "Brukernavn må oppgis")]
         public String UserName { get; set; }
+        [Required(ErrorMessage = "Må fylles ut!")]
         public byte[] Password { get; set; }
 
     }
